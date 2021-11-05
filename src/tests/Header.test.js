59b34f0975141055141bdd53d2158b6b8ee55bea
Lucas Duarte <lucs.rkt@gmail.com>
2021-11-05 18:41:42 -0300

    teste para requisito 10 hasNoHeader criado

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -19,3 +19,41 @@ describe('9 - Implemente os elementos do header na tela principal de receitas,'
     expect(searchTopBtn).toBeInTheDocument();
   });
 });
+
+describe('10 - Implemente um ícone para a tela de perfil,'
+  + 'um título e um ícone para a busca, caso exista no protótipo (hasNoHeader)', () => {
+  const hasNoHeader = () => {
+    expect(screen.getByTestId('profile-top-btn')).not.toBeInTheDocument();
+    expect(screen.getByTestId('page-title')).not.toBeInTheDocument();
+    expect(screen.getByTestId('search-top-btn')).not.toBeInTheDocument();
+  };
+
+  it('Não tem header na tela de login', () => {
+    renderWithRouter(<App />);
+    hasNoHeader();
+  });
+
+  it('Não tem header na tela de detalhes de uma receita de comida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas/52771');
+    hasNoHeader();
+  });
+
+  it('Não tem header na tela de detalhes de uma receita de bebida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/bebidas/178319');
+    hasNoHeader();
+  });
+
+  it('Não tem header na tela de receita em processo de comida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas/52771/in-progress');
+    hasNoHeader();
+  });
+
+  it('Não tem header na tela de receita em processo de bebida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/bebidas/178319/in-progress');
+    hasNoHeader();
+  });
+});
