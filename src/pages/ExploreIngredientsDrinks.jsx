76dd9c2ec4a11d5dc25eaa655fcba7c133037bef
Lucas Duarte <lucs.rkt@gmail.com>
2021-11-09 23:42:21 -0300

    Requisitos 19 ao 24 finalizados

--- src/pages/ExploreIngredientsDrinks.jsx
+++ src/pages/ExploreIngredientsDrinks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreIngredientsDrinks() {
   return (
     <div>
       <Header title="Explorar Ingredientes" hasBtn={ false } />
-      ExploreIngredientsDrinks
+
+      <Footer />
     </div>
   );
 }
