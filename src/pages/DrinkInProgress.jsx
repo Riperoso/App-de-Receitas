44010e78449b4598acd2c890cfc85419e567d55f
Samuel Nascimento <sfsnascimento@gmail.com>
2021-11-11 18:28:30 -0300

    Receita em progresso - requisitos 47 e 48

--- src/pages/DrinkInProgress.jsx
+++ src/pages/DrinkInProgress.jsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
-function DrinkInProgress() {
+function DrinkInProgress({ match: { params: { id } } }) {
+  const [api, saveApi] = useState({});
+  const apiFor = () => {
+    const ingredientsList = [];
+    const NUMBER_TWEENTY = 20;
+    if (api.drinks !== undefined) {
+      for (let index = 1; index < NUMBER_TWEENTY; index += 1) {
+        const str = `strIngredient${index}`;
+        ingredientsList.push(api.drinks[0][str]);
+      }
+    }
+    console.log(ingredientsList);
+    return ingredientsList;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const resolve = await response.json();
+      saveApi(resolve);
+    })();
+  }, [id]);
+
+  const drinkProgress = () => (
+    <div>
+      <img
+        src={ api.drinks[0].strDrinkThumb }
+        data-testid="recipe-photo"
+        alt="recipe name"
+      />
+      <h2 data-testid="recipe-title">{api.drinks[0].strDrink}</h2>
+      <button type="button" data-testid="share-btn">
+        <img src={ shareIcon } alt="botão de compartilhar" />
+      </button>
+      <button type="button" data-testid="favorite-btn">
+        <img src={ whiteHeartIcon } alt="botão de favoritar" />
+      </button>
+      <h4 data-testid="recipe-category">{api.drinks[0].strCategory}</h4>
+      { apiFor().map((ingredient, index) => (
+        ingredient !== '' && ingredient !== null && ingredient !== undefined && (
+          <label
+            data-testid={ `${index}-ingredient-step` }
+            htmlFor={ `${index}-ingredient-step` }
+            key={ index }
+          >
+            {ingredient}
+            <input
+              type="checkbox"
+              id={ `${index}-ingredient-step` }
+            />
+          </label>)
+      ))}
+      <h2>Instructions</h2>
+      <p data-testid="instructions">
+        {api.drinks[0].strInstructions}
+      </p>
+      <button type="button" data-testid="finish-recipe-btn">Finalizar receita</button>
+    </div>
+  );
+
   return (
     <div>
-      drink progress
+      { api.drinks !== undefined ? drinkProgress() : <h1>loading</h1> }
     </div>
   );
 }
+
+DrinkInProgress.propTypes = {
+  match: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default DrinkInProgress;
--- src/pages/FoodInProgress.jsx
+++ src/pages/FoodInProgress.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
-function FoodInProgress() {
+function FoodInProgress({ match: { params: { id } } }) {
+  const [api, saveApi] = useState({});
+  const apiFor = () => {
+    const ingredientsList = [];
+    const NUMBER_TWEENTY = 20;
+    if (api.meals !== undefined) {
+      for (let index = 1; index < NUMBER_TWEENTY; index += 1) {
+        const str = `strIngredient${index}`;
+        ingredientsList.push(api.meals[0][str]);
+      }
+    }
+    console.log(ingredientsList);
+    return ingredientsList;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const resolve = await response.json();
+      saveApi(resolve);
+    })();
+  }, [id]);
+
+  const foodProgress = () => (
+    <div>
+      <img
+        src={ api.meals[0].strMealThumb }
+        data-testid="recipe-photo"
+        alt="recipe name"
+      />
+      <h2 data-testid="recipe-title">{api.meals[0].strMeal}</h2>
+      <button type="button" data-testid="share-btn">
+        <img src={ shareIcon } alt="botão de compartilhar" />
+      </button>
+      <button type="button" data-testid="favorite-btn">
+        <img src={ whiteHeartIcon } alt="botão de favoritar" />
+      </button>
+      <h4 data-testid="recipe-category">{api.meals[0].strCategory}</h4>
+      <div>
+        { apiFor().map((ingredient, index) => (
+          ingredient !== '' && ingredient !== null && (
+            <label
+              data-testid={ `${index}-ingredient-step` }
+              htmlFor={ ingredient }
+              key={ `checkbox-${index}` }
+            >
+              {ingredient}
+              <input
+                type="checkbox"
+                id={ ingredient }
+              />
+            </label>)
+        ))}
+      </div>
+      <p data-testid="instructions">
+        <h2>Instructions</h2>
+        {api.meals[0].strInstructions}
+      </p>
+      <button type="button" data-testid="finish-recipe-btn">Finalizar receita</button>
+    </div>
+  );
+
   return (
     <div>
-      food progress
+      { api.meals !== undefined ? foodProgress() : <h1>loading</h1> }
     </div>
   );
 }
+
+FoodInProgress.propTypes = {
+  match: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default FoodInProgress;
