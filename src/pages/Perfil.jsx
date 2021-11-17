9d7f7e9fc0728b2197a4741c684fd76ff0f32853
Filipe <andradefilipe2002@gmail.com>
2021-11-16 21:21:02 -0300

    Tela de Perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Perfil() {
-  return (
-    <div>
-      <Header title="Perfil" hasBtn={ false } />
+  const history = useHistory();
+  const playerEmail = JSON.parse(localStorage.getItem('user')).email;
 
-      <Footer />
-    </div>
+  const onClick = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <>
+      <Header title="Perfil" hasBtn={ false } />
+      <div>
+        <p data-testid="profile-email">{ playerEmail }</p>
+        <Link to="/receitas-feitas">
+          <button type="button" data-testid="profile-done-btn">
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button type="button" data-testid="profile-favorite-btn">
+            Receitas Favoritas
+          </button>
+        </Link>
+        <button type="button" data-testid="profile-logout-btn" onClick={ onClick }>
+          Sair
+        </button>
+        <Footer />
+      </div>
+    </>
   );
 }
 
