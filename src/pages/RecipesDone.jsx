f138a256bfcb047ba0cfa5e7f6083294e2e7a5dc
Lucas Duarte <lucs.rkt@gmail.com>
2021-11-19 08:44:52 -0300

    Requisito 58 finalizado

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import RecipesDoneCard from '../components/RecipesDoneCard';
 
 function RecipesDone() {
+  const [filter, setFilter] = useState('all');
+
   const getStorage = () => {
     const storage = JSON.parse(localStorage.getItem('doneRecipes'));
     return storage;
@@ -12,13 +14,33 @@ function RecipesDone() {
     <div>
       <Header title="Receitas Feitas" hasBtn={ false } />
       <div>
-        <button data-testid="filter-by-food-btn" type="button">Food</button>
-        <button data-testid="filter-by-drink-btn" type="button">Drink</button>
-        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button
+          onClick={ () => setFilter('comida') }
+          data-testid="filter-by-food-btn"
+          type="button"
+        >
+          Food
+        </button>
+        <button
+          onClick={ () => setFilter('bebida') }
+          data-testid="filter-by-drink-btn"
+          type="button"
+        >
+          Drink
+        </button>
+        <button
+          onClick={ () => setFilter('all') }
+          data-testid="filter-by-all-btn"
+          type="button"
+        >
+          All
+        </button>
       </div>
-      {getStorage().map((rec, index) => (
-        <RecipesDoneCard key={ index } index={ index } recipe={ rec } />
-      ))}
+      {getStorage()
+        .filter((favorite) => (filter === 'all' ? favorite : favorite.type === filter))
+        .map((rec, index) => (
+          <RecipesDoneCard key={ index } index={ index } recipe={ rec } />
+        ))}
     </div>
   );
 }
